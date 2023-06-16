a45a2ee2b123e894a26d56d607763c3fb4d4ec9b
commandus <andrei.i.ivanov@commandus.com>
2023-06-16 15:14:11 +0900

    1

--- src/app/box-tree/box-tree.component.ts
+++ src/app/box-tree/box-tree.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { WebappService } from '../webapp.service';
 import { Box } from '../box.model';
+import { MatSelectionList } from '@angular/material/list';
 
 @Component({
   selector: 'app-box-tree',
@@ -8,7 +9,14 @@ import { Box } from '../box.model';
   styleUrls: ['./box-tree.component.css']
 })
 export class BoxTreeComponent {
+  // @ViewChild(MatSelectionList) boxlist: MatSelectionList;
+  @ViewChild('boxlist') boxlist: MatSelectionList | undefined;
+
   constructor(public svc: WebappService) { 
+    if (this.boxlist)
+      this.boxlist.selectedOptions.select(0 as any);
+    else
+    console.error('boxlist bind error');
   }
 
 }
