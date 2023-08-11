cb3daabfb5b23708e36e0b6ebe3206eb5da03458
Andrei <andrei.i.ivanov@gmail.com>
2023-08-11 15:30:23 +0900

    1

--- src/app/component-list/component-list.component.ts
+++ src/app/component-list/component-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges, ViewChild } from '@angular/core';
 import { WebappService } from '../webapp.service';
 import { MatSelectChange } from '@angular/material/select';
 import { Symbol } from '../model/symbol.model';
@@ -8,7 +8,7 @@ import { Symbol } from '../model/symbol.model';
   templateUrl: './component-list.component.html',
   styleUrls: ['./component-list.component.css']
 })
-export class ComponentListComponent {
+export class ComponentListComponent implements OnChanges {
   @Input() @Output() symbol_id: number = 0;
   @Input() @Output() symbol: Symbol = new Symbol;
   @Output() symbolSelected = new EventEmitter<Symbol>();
@@ -18,6 +18,11 @@ export class ComponentListComponent {
   constructor(public svc: WebappService) { 
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    this.update();
+  }
+
+
   public onSelectionChanged(event: MatSelectChange) {
     this.symbol = this.selectedSymbol;
     this.symbol_id = this.selectedSymbol.id;
@@ -25,6 +30,10 @@ export class ComponentListComponent {
   }
 
   ngOnInit(): void {
+    this.update();
+  }
+
+  private update(): void {
     if (this.symbol_id) {
       this.selectedSymbol = this.svc.getComponentById(this.symbol_id);
     } else {
@@ -33,5 +42,4 @@ export class ComponentListComponent {
       }
     }
   }
-
 }
